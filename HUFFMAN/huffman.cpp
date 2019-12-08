ef0560844c4ca871d147f46273c590c117e19558
Manvi Tyagi <manvityagi770@gmail.com>
2019-12-09 01:56:56 +0530

    sort

--- HUFFMAN/huffman.cpp
+++ HUFFMAN/huffman.cpp
@@ -1,299 +1,115 @@
-// C program for Huffman Coding 
-#include <stdio.h> 
-#include <stdlib.h> 
-
-// This constant can be avoided by explicitly 
-// calculating height of Huffman Tree 
-#define MAX_TREE_HT 100 
-
-// A Huffman tree node 
-struct MinHeapNode { 
-
-	// One of the input characters 
-	char data; 
-
-	// Frequency of the character 
-	unsigned freq; 
-
-	// Left and right child of this node 
-	struct MinHeapNode *left, *right; 
-}; 
-
-// A Min Heap: Collection of 
-// min-heap (or Huffman tree) nodes 
-struct MinHeap { 
-
-	// Current size of min heap 
-	unsigned size; 
-
-	// capacity of min heap 
-	unsigned capacity; 
-
-	// Array of minheap node pointers 
-	struct MinHeapNode** array; 
-}; 
-
-// A utility function allocate a new 
-// min heap node with given character 
-// and frequency of the character 
-struct MinHeapNode* newNode(char data, unsigned freq) 
-{ 
-	struct MinHeapNode* temp 
-		= (struct MinHeapNode*)malloc
-(sizeof(struct MinHeapNode)); 
-
-	temp->left = temp->right = NULL; 
-	temp->data = data; 
-	temp->freq = freq; 
-
-	return temp; 
-} 
-
-// A utility function to create 
-// a min heap of given capacity 
-struct MinHeap* createMinHeap(unsigned capacity) 
-
-{ 
-
-	struct MinHeap* minHeap 
-		= (struct MinHeap*)malloc(sizeof(struct MinHeap)); 
-
-	// current size is 0 
-	minHeap->size = 0; 
-
-	minHeap->capacity = capacity; 
-
-	minHeap->array 
-		= (struct MinHeapNode**)malloc(minHeap-> 
-capacity * sizeof(struct MinHeapNode*)); 
-	return minHeap; 
-} 
-
-// A utility function to 
-// swap two min heap nodes 
-void swapMinHeapNode(struct MinHeapNode** a, 
-					struct MinHeapNode** b) 
-
-{ 
-
-	struct MinHeapNode* t = *a; 
-	*a = *b; 
-	*b = t; 
-} 
-
-// The standard minHeapify function. 
-void minHeapify(struct MinHeap* minHeap, int idx) 
-
-{ 
-
-	int smallest = idx; 
-	int left = 2 * idx + 1; 
-	int right = 2 * idx + 2; 
-
-	if (left < minHeap->size && minHeap->array[left]-> 
-freq < minHeap->array[smallest]->freq) 
-		smallest = left; 
-
-	if (right < minHeap->size && minHeap->array[right]-> 
-freq < minHeap->array[smallest]->freq) 
-		smallest = right; 
-
-	if (smallest != idx) { 
-		swapMinHeapNode(&minHeap->array[smallest], 
-						&minHeap->array[idx]); 
-		minHeapify(minHeap, smallest); 
-	} 
-} 
-
-// A utility function to check 
-// if size of heap is 1 or not 
-int isSizeOne(struct MinHeap* minHeap) 
-{ 
-
-	return (minHeap->size == 1); 
-} 
-
-// A standard function to extract 
-// minimum value node from heap 
-struct MinHeapNode* extractMin(struct MinHeap* minHeap) 
-
-{ 
-
-	struct MinHeapNode* temp = minHeap->array[0]; 
-	minHeap->array[0] 
-		= minHeap->array[minHeap->size - 1]; 
-
-	--minHeap->size; 
-	minHeapify(minHeap, 0); 
-
-	return temp; 
-} 
-
-// A utility function to insert 
-// a new node to Min Heap 
-void insertMinHeap(struct MinHeap* minHeap, 
-				struct MinHeapNode* minHeapNode) 
-
-{ 
-
-	++minHeap->size; 
-	int i = minHeap->size - 1; 
-
-	while (i && minHeapNode->freq < minHeap->array[(i - 1) / 2]->freq) { 
-
-		minHeap->array[i] = minHeap->array[(i - 1) / 2]; 
-		i = (i - 1) / 2; 
-	} 
-
-	minHeap->array[i] = minHeapNode; 
-} 
-
-// A standard function to build min heap 
-void buildMinHeap(struct MinHeap* minHeap) 
-
-{ 
-
-	int n = minHeap->size - 1; 
-	int i; 
-
-	for (i = (n - 1) / 2; i >= 0; --i) 
-		minHeapify(minHeap, i); 
-} 
-
-// A utility function to print an array of size n 
-void printArr(int arr[], int n) 
-{ 
-	int i; 
-	for (i = 0; i < n; ++i) 
-		printf("%d", arr[i]); 
-
-	printf("\n"); 
-} 
-
-// Utility function to check if this node is leaf 
-int isLeaf(struct MinHeapNode* root) 
-
-{ 
-
-	return !(root->left) && !(root->right); 
-} 
-
-// Creates a min heap of capacity 
-// equal to size and inserts all character of 
-// data[] in min heap. Initially size of 
-// min heap is equal to capacity 
-struct MinHeap* createAndBuildMinHeap(char data[], int freq[], int size) 
-
-{ 
-
-	struct MinHeap* minHeap = createMinHeap(size); 
-
-	for (int i = 0; i < size; ++i) 
-		minHeap->array[i] = newNode(data[i], freq[i]); 
-
-	minHeap->size = size; 
-	buildMinHeap(minHeap); 
-
-	return minHeap; 
-} 
-
-// The main function that builds Huffman tree 
-struct MinHeapNode* buildHuffmanTree(char data[], int freq[], int size) 
-
-{ 
-	struct MinHeapNode *left, *right, *top; 
-
-	// Step 1: Create a min heap of capacity 
-	// equal to size. Initially, there are 
-	// modes equal to size. 
-	struct MinHeap* minHeap = createAndBuildMinHeap(data, freq, size); 
-
-	// Iterate while size of heap doesn't become 1 
-	while (!isSizeOne(minHeap)) { 
-
-		// Step 2: Extract the two minimum 
-		// freq items from min heap 
-		left = extractMin(minHeap); 
-		right = extractMin(minHeap); 
-
-		// Step 3: Create a new internal 
-		// node with frequency equal to the 
-		// sum of the two nodes frequencies. 
-		// Make the two extracted node as 
-		// left and right children of this new node. 
-		// Add this node to the min heap 
-		// '$' is a special value for internal nodes, not used 
-		top = newNode('$', left->freq + right->freq); 
-
-		top->left = left; 
-		top->right = right; 
-
-		insertMinHeap(minHeap, top); 
-	} 
-
-	// Step 4: The remaining node is the 
-	// root node and the tree is complete. 
-	return extractMin(minHeap); 
-} 
-
-// Prints huffman codes from the root of Huffman Tree. 
-// It uses arr[] to store codes 
-void printCodes(struct MinHeapNode* root, int arr[], int top) 
-
-{ 
-
-	// Assign 0 to left edge and recur 
-	if (root->left) { 
-
-		arr[top] = 0; 
-		printCodes(root->left, arr, top + 1); 
-	} 
-
-	// Assign 1 to right edge and recur 
-	if (root->right) { 
-
-		arr[top] = 1; 
-		printCodes(root->right, arr, top + 1); 
-	} 
-
-	// If this is a leaf node, then 
-	// it contains one of the input 
-	// characters, print the character 
-	// and its code from arr[] 
-	if (isLeaf(root)) { 
-
-		printf("%c: ", root->data); 
-		printArr(arr, top); 
-	} 
-} 
-
-// The main function that builds a 
-// Huffman Tree and print codes by traversing 
-// the built Huffman Tree 
-void HuffmanCodes(char data[], int freq[], int size) 
-
-{ 
-	// Construct Huffman Tree 
-	struct MinHeapNode* root 
-		= buildHuffmanTree(data, freq, size); 
-
-	// Print Huffman codes using 
-	// the Huffman tree built above 
-	int arr[MAX_TREE_HT], top = 0; 
-
-	printCodes(root, arr, top); 
-} 
-
-// Driver program to test above functions 
-int main() 
-{ 
-
-	char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' }; 
-	int freq[] = { 5, 9, 12, 13, 16, 45 }; 
-
-	int size = sizeof(arr) / sizeof(arr[0]); 
-
-	HuffmanCodes(arr, freq, size); 
-
-	return 0; 
-} 
+#include <iostream>
+#include <queue>
+#include <map>
+#include <climits> // for CHAR_BIT
+#include <iterator>
+#include <algorithm>
+ 
+const int UniqueSymbols = 1 << CHAR_BIT;
+const char* SampleString = "this is an example for huffman encoding";
+ 
+typedef std::vector<bool> HuffCode;
+typedef std::map<char, HuffCode> HuffCodeMap;
+ 
+class INode
+{
+public:
+    const int f;
+ 
+    virtual ~INode() {}
+ 
+protected:
+    INode(int f) : f(f) {}
+};
+ 
+class InternalNode : public INode
+{
+public:
+    INode *const left;
+    INode *const right;
+ 
+    InternalNode(INode* c0, INode* c1) : INode(c0->f + c1->f), left(c0), right(c1) {}
+    ~InternalNode()
+    {
+        delete left;
+        delete right;
+    }
+};
+ 
+class LeafNode : public INode
+{
+public:
+    const char c;
+ 
+    LeafNode(int f, char c) : INode(f), c(c) {}
+};
+ 
+struct NodeCmp
+{
+    bool operator()(const INode* lhs, const INode* rhs) const { return lhs->f > rhs->f; }
+};
+ 
+INode* BuildTree(const int (&frequencies)[UniqueSymbols])
+{
+    std::priority_queue<INode*, std::vector<INode*>, NodeCmp> trees;
+ 
+    for (int i = 0; i < UniqueSymbols; ++i)
+    {
+        if(frequencies[i] != 0)
+            trees.push(new LeafNode(frequencies[i], (char)i));
+    }
+    while (trees.size() > 1)
+    {
+        INode* childR = trees.top();
+        trees.pop();
+ 
+        INode* childL = trees.top();
+        trees.pop();
+ 
+        INode* parent = new InternalNode(childR, childL);
+        trees.push(parent);
+    }
+    return trees.top();
+}
+ 
+void GenerateCodes(const INode* node, const HuffCode& prefix, HuffCodeMap& outCodes)
+{
+    if (const LeafNode* lf = dynamic_cast<const LeafNode*>(node))
+    {
+        outCodes[lf->c] = prefix;
+    }
+    else if (const InternalNode* in = dynamic_cast<const InternalNode*>(node))
+    {
+        HuffCode leftPrefix = prefix;
+        leftPrefix.push_back(false);
+        GenerateCodes(in->left, leftPrefix, outCodes);
+ 
+        HuffCode rightPrefix = prefix;
+        rightPrefix.push_back(true);
+        GenerateCodes(in->right, rightPrefix, outCodes);
+    }
+}
+ 
+int main()
+{
+    // Build frequency table
+    int frequencies[UniqueSymbols] = {0};
+    const char* ptr = SampleString;
+    while (*ptr != '\0')
+        ++frequencies[*ptr++];
+ 
+    INode* root = BuildTree(frequencies);
+ 
+    HuffCodeMap codes;
+    GenerateCodes(root, HuffCode(), codes);
+    delete root;
+ 
+    for (HuffCodeMap::const_iterator it = codes.begin(); it != codes.end(); ++it)
+    {
+        std::cout << it->first << " ";
+        std::copy(it->second.begin(), it->second.end(),
+                  std::ostream_iterator<bool>(std::cout));
+        std::cout << std::endl;
+    }
+    return 0;
+}
--- SORTING/radix.cpp
+++ SORTING/radix.cpp
@@ -1,54 +1,73 @@
-// C++ implementation of Shell Sort 
-#include <iostream> 
+// C++ implementation of Radix Sort 
+#include<iostream> 
 using namespace std; 
 
-/* function to sort arr using shellSort */
-int shellSort(int arr[], int n) 
+// A utility function to get maximum value in arr[] 
+int getMax(int arr[], int n) 
 { 
-	// Start with a big gap, then reduce the gap 
-	for (int gap = n/2; gap > 0; gap /= 2) 
-	{ 
-		// Do a gapped insertion sort for this gap size. 
-		// The first gap elements a[0..gap-1] are already in gapped order 
-		// keep adding one more element until the entire array is 
-		// gap sorted 
-		for (int i = gap; i < n; i += 1) 
-		{ 
-			// add a[i] to the elements that have been gap sorted 
-			// save a[i] in temp and make a hole at position i 
-			int temp = arr[i]; 
-
-			// shift earlier gap-sorted elements up until the correct 
-			// location for a[i] is found 
-			int j;			 
-			for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
-				arr[j] = arr[j - gap]; 
-			
-			// put temp (the original a[i]) in its correct location 
-			arr[j] = temp; 
-		} 
-	} 
-	return 0; 
+	int mx = arr[0]; 
+	for (int i = 1; i < n; i++) 
+		if (arr[i] > mx) 
+			mx = arr[i]; 
+	return mx; 
 } 
 
-void printArray(int arr[], int n) 
+// A function to do counting sort of arr[] according to 
+// the digit represented by exp. 
+void countSort(int arr[], int n, int exp) 
 { 
-	for (int i=0; i<n; i++) 
+	int output[n]; // output array 
+	int i, count[10] = {0}; 
+
+	// Store count of occurrences in count[] 
+	for (i = 0; i < n; i++) 
+		count[ (arr[i]/exp)%10 ]++; 
+
+	// Change count[i] so that count[i] now contains actual 
+	// position of this digit in output[] 
+	for (i = 1; i < 10; i++) 
+		count[i] += count[i - 1]; 
+
+	// Build the output array 
+	for (i = n - 1; i >= 0; i--) 
+	{ 
+		output[count[ (arr[i]/exp)%10 ] - 1] = arr[i]; 
+		count[ (arr[i]/exp)%10 ]--; 
+	} 
+
+	// Copy the output array to arr[], so that arr[] now 
+	// contains sorted numbers according to current digit 
+	for (i = 0; i < n; i++) 
+		arr[i] = output[i]; 
+} 
+
+// The main function to that sorts arr[] of size n using 
+// Radix Sort 
+void radixsort(int arr[], int n) 
+{ 
+	// Find the maximum number to know number of digits 
+	int m = getMax(arr, n); 
+
+	// Do counting sort for every digit. Note that instead 
+	// of passing digit number, exp is passed. exp is 10^i 
+	// where i is current digit number 
+	for (int exp = 1; m/exp > 0; exp *= 10) 
+		countSort(arr, n, exp); 
+} 
+
+// A utility function to print an array 
+void print(int arr[], int n) 
+{ 
+	for (int i = 0; i < n; i++) 
 		cout << arr[i] << " "; 
 } 
 
+// Driver program to test above functions 
 int main() 
 { 
-	int arr[] = {12, 34, 54, 2, 3}, i; 
+	int arr[] = {170, 45, 75, 90, 802, 24, 2, 66}; 
 	int n = sizeof(arr)/sizeof(arr[0]); 
-
-	cout << "Array before sorting: \n"; 
-	printArray(arr, n); 
-
-	shellSort(arr, n); 
-
-	cout << "\nArray after sorting: \n"; 
-	printArray(arr, n); 
-
+	radixsort(arr, n); 
+	print(arr, n); 
 	return 0; 
 } 
